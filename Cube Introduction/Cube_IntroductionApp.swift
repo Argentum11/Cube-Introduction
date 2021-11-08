b709e6b50e0d8d20a7964b16009a554362d7ee5c
jubao <tqy_4896@hotmail.com>
2021-11-08 09:39:19 +0800

    Initial Commit

---
+++ Cube Introduction/Cube_IntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cube_IntroductionApp.swift
+//  Cube Introduction
+//
+//  Created by User16 on 2021/11/8.
+//
+
+import SwiftUI
+
+@main
+struct Cube_IntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
